7da9766dbd1bba641ddfc72d5bebd13298e72f2c
FawnKitten <joaopollo@gmail.com>
2022-10-11 10:58:06 -0700

    added translations

---
+++ app/src/main/java/com/example/trivia/Quiz.kt
@@ -0,0 +1,65 @@
+package com.example.trivia
+
+import android.util.Log
+
+class Quiz(): Iterable<Question> {
+    var questions: List<Question> = listOf()
+    var currentQuestion = 0
+    var score = 0
+
+    companion object {
+        const val TAG = "Quiz"
+    }
+
+    fun loadQuestions(questions: List<Question>) {
+        this.questions = questions
+    }
+
+    fun isAtEnd(): Boolean {
+        return currentQuestion < questions.size
+    }
+
+    fun nextQuestion(): Question {
+        val question = questions[currentQuestion]
+        currentQuestion++
+        return question
+    }
+
+//    fun getNextQuestion(): Question? {
+//        return questions.getOrNull(currentQuestion).also {
+//            currentQuestion++
+//        }
+//    }
+
+    fun getCurrentQuestion(): Question {
+        return questions[currentQuestion]
+    }
+
+    fun choseAnswer(chosen: String) {
+        Log.d(TAG, "choseAnswer: chosen=$chosen correct=${questions[currentQuestion].correct}")
+        if (chosen == questions[currentQuestion].correct) {
+            score++
+            currentQuestion++
+        }
+    }
+
+
+    /**
+     * NOTE: from this point on is iterator stuff, DON'T WORRY ABOUT THIS DAVID
+     */
+    operator fun next(): Question =
+        questions[currentQuestion].also {
+            currentQuestion++
+        }
+
+    operator fun hasNext(): Boolean =
+        currentQuestion < questions.size
+
+    /**
+     * Returns an iterator over the elements of this object.
+     */
+    override fun iterator(): Iterator<Question> {
+        return questions.iterator()
+    }
+
+}
